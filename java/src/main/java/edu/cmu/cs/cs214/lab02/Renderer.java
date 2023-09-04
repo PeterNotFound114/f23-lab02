ddeafe61d3270da7433e9ad864c8145930201cf8
PeterNotFound114 <sicheng2@andrew.cmu.edu>
2023-09-04 19:13:23 -0400

    Specified interface for shape

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -4,10 +4,10 @@ import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Renderer {
     public Rectangle rectangle;
-    
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
-    }
+
+    Renderer(Shape shape) {
+        this.shape = shape;
+    } // Change to shape
 
     void draw() {
         double area = rectangle.getArea();
